a74e914f40c3e1255008592e65a135b95ecaa219
ecabigting <ericcabigting@outlook.com>
2021-04-27 19:57:17 +0400

    - update sanity version - update content editor

--- cmsh/schemas/author.js
+++ cmsh/schemas/author.js
@@ -33,9 +33,7 @@ export default {
         {
           title: 'Block',
           type: 'block',
-          styles: [{title: 'Normal', value: 'normal'}],
-          lists: [],
-        },
+        }
       ],
     },
   ],
