109c025781199513abd0e2606f16ea45df4ac11d
Eric Thomas D. Cabigting <thomas@ericcabigting.dev>
2026-04-05 09:37:29 +0400

    recent update

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,13 +1,12 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'o2jpyk3u',
-    dataset: 'production'
+    dataset: 'production',
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
+  reactStrictMode: true,
+  deployment: {
+    autoUpdates: true
+  }
 })
